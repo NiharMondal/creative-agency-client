200ec1d12a8812b5cbe77ff95e932c384c8f2183
Nihar Mondal <niharmondol9@gmail.com>
2021-03-10 17:23:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from 'react';
+import {
+  BrowserRouter as Router,
 
+  Route, Switch
+} from "react-router-dom";
+import './App.css';
+import Auth from './components/Auth/Auth';
+import Home from './components/Home/Home';
+
+export const UserContext = createContext();
 function App() {
+  const [user,setUser]=useState({})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={[user,setUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+              <Home/>
+          </Route>
+          <Route path="/auth">
+            <Auth/>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Auth.css'
+const Auth = () => {
+   return (
+      <div>
+         <h1>I am auth</h1>
+      </div>
+   );
+};
+
+export default Auth;
